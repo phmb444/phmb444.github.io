534404cbe14635b8536d852237a3cf14deaa352d
Pedro Haubert <pedrohaubert7@gmail.com>
2023-09-17 19:52:56 -0300

    aaaa

--- app.js
+++ app.js
@@ -1,25 +1,13 @@
-const token = 'ae6c6b26e319468389619f74c5c59613';
-async function fetchWebApi(endpoint, method, body) {
-  const res = await fetch(`https://api.spotify.com/${endpoint}`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-    method,
-    body:JSON.stringify(body)
-  });
-  return await res.json();
-}
+const express = require('express');
+const app = express();
+const port = 3000;
 
-async function getTopTracks(){
-    // Endpoint reference : https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks
-    return (await fetchWebApi(
-      'v1/me/top/tracks?time_range=short_term&limit=5', 'GET'
-    )).items;
-  }
+app.use(express.static('public'));
 
-async function getTopTracks(){
-    // Endpoint reference : https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks
-    return (await fetchWebApi(
-      'v1/me/top/tracks?time_range=short_term&limit=5', 'GET'
-    )).items;
-}
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html'); // Substitua 'index.html' pelo nome do seu arquivo HTML, se for diferente.
+});
+
+app.listen(port, () => {
+  console.log(`Servidor está executando em http://localhost:${port}`);
+});
